e34d0f66ea498031ed4915d062b27e911e509987
David Faulkner <davfaulk@amazon.com>
2017-01-05 01:05:29 +1300

    Added CRUD calls for Tournaments

--- konpe.py
+++ konpe.py
@@ -1,12 +1,17 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from flask_restful import reqparse
-from sqlalchemy import create_engine
 from json import dumps
+import logging
+import sqlite3
 
-#Create a engine for connecting to SQLite3.
-#Assuming salaries.db is in your app root folder
-e = create_engine('sqlite:///konpe.db')
+#logging.basicConfig(level=logging.DEBUG, filename='konpe_debug.log')
+
+#connecting to SQLite3.
+sql = sqlite3.connect('konpe.db')
+sql.row_factory = sqlite3.Row
+#Turn off transactions for increased performance, commit() not required
+sql.isolation_level = None
 
 app = Flask(__name__)
 api = Api(app)
@@ -22,83 +27,117 @@ TIMEKUMITE = 4
 
 class Tournament(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select * from tournaments")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
-        return result
-
-class Participant(Resource):
-    def get(self):
-        conn = e.connect()
-        query = conn.execute("select * from tournaments")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select * from tournaments")
+        rows = cur.fetchall()
+        cols = rows[0].keys()
+        result = {'data': [dict(zip(tuple (cols) ,i)) for i in rows]}
         return result
 
     def post(self):
         try:
-            # Parse the arguments
             parser = reqparse.RequestParser()
-            parser.add_argument('first_name', type=str, help='Email address to create user')
-            parser.add_argument('password', type=str, help='Password to create user')
+            parser.add_argument('TournamentName', type=str)
+            parser.add_argument('TournamentDate', type=str)
+            parser.add_argument('TournamentLocation', type=str)
+            parser.add_argument('TournamentRings', type=int)
             args = parser.parse_args()
 
-            return {'Email': args['email'], 'Password': args['password']}
-
+            cur = sql.execute\
+                ('INSERT INTO Tournaments (TournamentName, TournamentDate, TournamentLocation, TournamentRings) VALUES'
+                 '(:TournamentName, :TournamentDate, :TournamentLocation, :TournamentRings);',
+                 args)
+            logging.debug(cur.lastrowid)
+            return {'TournamentID': cur.lastrowid}
         except Exception as e:
-            return {'error': str(e)}
+            return {'error': str(e)}, 500
+
+    def put(self):
+        try:
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('TournamentID', type=str, required=True)
+            parser.add_argument('TournamentName', type=str)
+            parser.add_argument('TournamentDate', type=str)
+            parser.add_argument('TournamentLocation', type=str)
+            parser.add_argument('TournamentRings', type=int)
+            args = parser.parse_args()
+
+            cur = sql.execute\
+                ('UPDATE Tournaments SET TournamentName=:TournamentName, TournamentDate=:TournamentDate,'
+                 'TournamentLocation=:TournamentLocation, TournamentRings=:TournamentRings '
+                 'WHERE TournamentID = :TournamentID;',
+                 args)
+            return {'TournamentID': args['TournamentID']}
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+    def delete(self):
+        try:
+            # Parse the arguments
+            parser = reqparse.RequestParser()
+            parser.add_argument('TournamentID', type=int)
+            args = parser.parse_args()
+            cur = sql.execute\
+                ('DELETE FROM Tournaments WHERE TournamentID = :TournamentID;',
+                 args)
+            return {'TournamentID': args['TournamentID']}
+        except Exception as e:
+            return {'error': str(e)}, 500
+
+class Participant(Resource):
+    def get(self):
+        cur = sql.execute("select * from tournaments")
+        rows = cur.fetchall()
+        cols = rows[0].keys()
+        result = {'data': [dict(zip(tuple (cols) ,i)) for i in rows]}
+        return result
+
+    def post(self):
+        pass
 
 class Division(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select * from tournaments")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select * from tournaments")
+        rows = cur.fetchall()
+        cols = rows[0].keys()
+        result = {'data': [dict(zip(tuple (cols) ,i)) for i in rows]}
         return result
 
 class DivisionParticipants(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select * from tournaments")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select * from tournaments")
+        rows = cur.fetchall()
+        cols = rows[0].keys()
+        result = {'data': [dict(zip(tuple (cols) ,i)) for i in rows]}
         return result
 
 class ConfigDojo(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select Value from Config where Key = 'Dojos'")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select Value from Config where Key = 'Dojos'")
+        result = {'data': cur.fetchone()['Value']}
         return result
 
 class ConfigRegion(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select Value from Config where Key = 'Countries'")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select Value from Config where Key = 'Regions'")
+        result = {'data': cur.fetchone()['Value']}
         return result
 
 class ConfigCountry(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select Value from Config where Key = 'Regions'")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select Value from Config where Key = 'Countries'")
+        result = {'data': cur.fetchone()['Value']}
         return result
 
 class ConfigGrade(Resource):
     def get(self):
-        conn = e.connect()
-        query = conn.execute("select Value from Config where Key = 'Grades'")
-        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
-        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        cur = sql.execute("select Value from Config where Key = 'Grades'")
+        result = {'data': cur.fetchone()['Value']}
         return result
 
-api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
-api.add_resource(Departments_Meta, '/departments')
+api.add_resource(Tournament, '/Tournament')
+api.add_resource(ConfigRegion, '/Config/Region')
+
 
 if __name__ == '__main__':
      app.run()
